36633329baaab774d96c8e48480cc013c1447922
Elilucio7 <eliluciofilho@gmail.com>
2026-02-08 20:49:56 -0300

    Update classe Jogo
    alteração da descrição e atributos da classe Jogo, com base na UML
    
    Co-Authored-By: Icaro Cavalcante <203451841+Icaro-Cavalcante@users.noreply.github.com>

--- src/board_and_play_poo/jogos.py
+++ src/board_and_play_poo/jogos.py
@@ -1,16 +1,12 @@
 from .produtos import Produto
 
 class Jogo(Produto):
-    '''Descreve os atributos e métodos gerais que existem em todas as especificações de jogos na loja, ajuda quanto ao controle de criação de instâncias e atributos em toda especificação.'''
-    def __init__(self, id, nome, preco, descricao, categoria, idade_min, pub_recomendado, tipo, esta_ativo = True):
-        super().__init__(id, nome, preco)
-        self.__descricao = descricao
-        self.__categoria = categoria
-        self.__idade_min = idade_min
-        self.__pub_recomendado = pub_recomendado
-        self.__esta_ativo = esta_ativo
-        self.__tipo = tipo
-
-    def deletar():
-            '''muda status para inativo'''
-            pass
+    '''Descreve os atributos que existem em todas as especificações de jogos na loja, funciona como classe abstrata, a qual os atributos vão ser herdados pelas classes Jogo_aluguel e Jogo_venda.'''
+    def __init__(self, id, nome, custo_aquisicao, data_aquisicao, descricao, idade_min, num_jogadores, tipo, status):
+        super().__init__(id, nome, data_aquisicao)
+        self._descricao = descricao
+        self._idade_min = idade_min
+        self._custo_aquisicao = custo_aquisicao
+        self._num_jogadores = num_jogadores
+        self._status = status
+        self._tipo = tipo
