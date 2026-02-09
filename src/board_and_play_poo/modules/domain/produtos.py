347cc20c7779b7711c05796b791bbc0de1e57351
Icaro-Cavalcante <icarodev06@gmail.com>
2026-02-08 23:04:10 -0300

    Produtos
    - `Implementação dos atributos que estão na UML na classe produto`
    - `Criação do método pasta_database, o qual cria a pasta onde vão estar os bancos de dados do sistema`
    - `Docstings explicando métodos e a classe`
    
    Co-Authored-By: Jackson <230462551+SJacksonML@users.noreply.github.com>

--- src/board_and_play_poo/modules/domain/produtos.py
+++ src/board_and_play_poo/modules/domain/produtos.py
@@ -1,7 +1,15 @@
-class Produto:
-    '''Descreve um produto'''
+from pathlib import Path
+caminho_data_pasta = r"src/board_and_play_poo/data"
 
-    def __init__(self, id, nome, preco):
-        self.__nome = nome
-        self.__id = id
-        self.preco = preco
+class Produto:
+    '''Integra atributos em comum de suas subclasses, mantendo controle do atributo 'custo' para verificar valor de operação/despesa'''
+
+    def __init__(self, id, nome, custo_aquisicao):
+        self._nome = nome
+        self._id = id
+        self._custo_aquisicao = custo_aquisicao
+
+    def pasta_database():
+        '''Método que cria a pasta (data) de dados, caso ela não exista'''
+        data_dir = Path(caminho_data_pasta)
+        data_dir.mkdir(exist_ok=True)
