4baa865efcceafb328650ef23a1db00e91793ff6
Elilucio7 <eliluciofilho@gmail.com>
2026-02-10 10:27:20 -0300

    mudança de id para id_produto
    os ids de jogos alugaveis e jogos compraveis ainda não estavam atualizados para id_produto

--- src/board_and_play_poo/modules/domain/alugaveis.py
+++ src/board_and_play_poo/modules/domain/alugaveis.py
@@ -6,23 +6,23 @@ class Jogo_aluguel(Jogo):
     '''
     A classe dos jogos alugáveis. Ela cuida do CRUD 
     '''
-    def __init__(self, id, nome, custo_aquisicao, data_aquisicao, descricao, idade_min, num_jogadores, tipo, status, valor_sessao, valor_diaria):
-        super().__init__(id, nome, custo_aquisicao, data_aquisicao, descricao, idade_min, num_jogadores, tipo, status)
+    def __init__(self, id_produto, nome, custo_aquisicao, data_aquisicao, descricao, idade_min, num_jogadores, tipo, status, valor_sessao, valor_diaria):
+        super().__init__(id_produto, nome, custo_aquisicao, data_aquisicao, descricao, idade_min, num_jogadores, tipo, status)
         self._valor_sessao = valor_sessao
         self._valor_diaria = valor_diaria
 
     def __str__(self):
-         return f"ID: {self.id}\nNome: {self.nome}\nCusto de aquisição: {self._custo_aquisicao}\nDescrição: {self._descricao}\nIdade mínima: {self._idade_min}\nNúmero de jogadores: {self._num_jogadores}\nTipo: {self.tipo}\nStatus: {self._status}"
+         return f"ID: {self.id_produto}\nNome: {self.nome}\nCusto de aquisição: {self._custo_aquisicao}\nDescrição: {self._descricao}\nIdade mínima: {self._idade_min}\nNúmero de jogadores: {self._num_jogadores}\nTipo: {self.tipo}\nStatus: {self._status}"
 
     def __eq__(self, outro):
-         return self.id == outro.id
+         return self.id_produto == outro.id_produto
     
     def tabela_jogo_aluguel():
       '''Cria a tabela de jogos alugáveis no banco de dados'''
       Jogo_aluguel.pasta_database()
       conexao = sqlite3.connect(caminho_data)
       cursor = conexao.cursor()
-      cursor.execute('''CREATE TABLE IF NOT EXISTS jogo_aluguel (id INTEGER UNIQUE, nome TEXT, custo_aquisicao REAL, data_aquisicao TEXT, descricao TEXT, idade_min INTEGER, num_jogadores INTEGER, tipo TEXT, status TEXT, valor_sessao REAL, valor_diaria REAL)''')
+      cursor.execute('''CREATE TABLE IF NOT EXISTS jogo_aluguel (id_produto INTEGER UNIQUE, nome TEXT, custo_aquisicao REAL, data_aquisicao TEXT, descricao TEXT, idade_min INTEGER, num_jogadores INTEGER, tipo TEXT, status TEXT, valor_sessao REAL, valor_diaria REAL)''')
 
       cursor.close()
       conexao.close()
@@ -47,47 +47,47 @@ class Jogo_aluguel(Jogo):
          conexao = sqlite3.connect(caminho_data)
          cursor = conexao.cursor()
          cursor.execute('''INSERT OR IGNORE INTO jogo_aluguel
-                       (id, nome, custo_aquisicao, data_aquisicao, descricao, idade_min, num_jogadores, tipo, status, valor_sessao, valor_diaria)
-                       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', (novo_jogo._id, novo_jogo._nome, novo_jogo._custo_aquisicao, novo_jogo._data_aquisicao, novo_jogo._descricao, novo_jogo._idade_min, novo_jogo._num_jogadores, novo_jogo._tipo, novo_jogo._status, novo_jogo._valor_sessao, novo_jogo._valor_diaria))
+                       (id_produto, nome, custo_aquisicao, data_aquisicao, descricao, idade_min, num_jogadores, tipo, status, valor_sessao, valor_diaria)
+                       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', (novo_jogo._id_produto, novo_jogo._nome, novo_jogo._custo_aquisicao, novo_jogo._data_aquisicao, novo_jogo._descricao, novo_jogo._idade_min, novo_jogo._num_jogadores, novo_jogo._tipo, novo_jogo._status, novo_jogo._valor_sessao, novo_jogo._valor_diaria))
          conexao.commit()
          cursor.close()
          conexao.close()
 
          print("\n Jogo criado\n")
 
-    def read(id):
+    def read(id_produto):
           '''Recebe o id do jogo alugável e retorna uma tupla com os dados dele.'''
           Jogo_aluguel.tabela_jogo_aluguel()
           conexao = sqlite3.connect(caminho_data)
           cursor = conexao.cursor()
-          cursor.execute('''SELECT * FROM jogo_aluguel WHERE id = ?''', (id, ))
+          cursor.execute('''SELECT * FROM jogo_aluguel WHERE id_produto = ?''', (id_produto, ))
           jogo_alugavel = cursor.fetchone()
 
           cursor.close()
           conexao.close()
           return jogo_alugavel
     
-    def update(id, nome_atributo, atributo_update):
+    def update(id_produto, nome_atributo, atributo_update):
           '''Recebe o id, o nome do atributo e o atributo update e atualiza o atributo com o  valor do update.'''
           Jogo_aluguel.tabela_jogo_aluguel()
           conexao = sqlite3.connect(caminho_data)
           cursor = conexao.cursor()
           cursor.execute(f'''UPDATE jogo_aluguel
                         SET {nome_atributo} = ?
-                        WHERE id = ?''', (atributo_update, id))
+                        WHERE id_produto = ?''', (atributo_update, id_produto))
           
           conexao.commit()
           cursor.close()
           conexao.close() 
     
-    def delete(id):
+    def delete(id_produto):
           '''Recebe o id do jogo alugável e atualiza seu status para inativo.'''
           Jogo_aluguel.tabela_jogo_aluguel()
           conexao = sqlite3.connect(caminho_data)
           cursor = conexao.cursor()
           cursor.execute('''UPDATE jogo_aluguel
                         SET status = ?
-                        WHERE id = ?''', ("inativo", id))
+                        WHERE id_produto = ?''', ("inativo", id_produto))
           
           conexao.commit()
           cursor.close()
--- src/board_and_play_poo/modules/domain/compraveis.py
+++ src/board_and_play_poo/modules/domain/compraveis.py
@@ -6,22 +6,22 @@ class Jogo_venda(Jogo):
     '''
     A classe dos jogos compráveis. Ela cuida do CRUD 
     '''
-    def __init__(self, id_jogo_venda, nome, custo_aquisicao, data_aquisicao, descricao, idade_min, num_jogadores, tipo, status, valor_compra):
-          super().__init__(id, nome, custo_aquisicao, data_aquisicao, descricao, idade_min, num_jogadores, tipo, status)
+    def __init__(self, id_produto, nome, custo_aquisicao, data_aquisicao, descricao, idade_min, num_jogadores, tipo, status, valor_compra):
+          super().__init__(id_produto, nome, custo_aquisicao, data_aquisicao, descricao, idade_min, num_jogadores, tipo, status)
           self._valor_compra = valor_compra
 
     def __str__(self):
-         return f"ID: {self.id}\nNome: {self.nome}\nCusto de aquisição: {self._custo_aquisicao}\nDescrição: {self._descricao}\nIdade mínima: {self._idade_min}\nNúmero de jogadores: {self._num_jogadores}\nTipo: {self.tipo}\nStatus: {self._status}"
+         return f"ID: {self.id_produto}\nNome: {self.nome}\nCusto de aquisição: {self._custo_aquisicao}\nDescrição: {self._descricao}\nIdade mínima: {self._idade_min}\nNúmero de jogadores: {self._num_jogadores}\nTipo: {self.tipo}\nStatus: {self._status}"
 
     def __eq__(self, outro):
-         return self.id == outro.id
+         return self.id_produto == outro.id_produto
     
     def tabela_jogo_venda():
       '''Cria a tabela de jogos alugáveis no banco de dados'''
       Jogo_venda.pasta_database()
       conexao = sqlite3.connect(caminho_data)
       cursor = conexao.cursor()
-      cursor.execute('''CREATE TABLE IF NOT EXISTS jogo_compra(id INTEGER UNIQUE, nome TEXT, custo_aquisicao REAL, data_aquisicao TEXT, descricao TEXT, idade_min INTEGER, num_jogadores INTEGER, tipo TEXT, status TEXT, valor_compra REAL)''')
+      cursor.execute('''CREATE TABLE IF NOT EXISTS jogo_compra(id_produto INTEGER UNIQUE, nome TEXT, custo_aquisicao REAL, data_aquisicao TEXT, descricao TEXT, idade_min INTEGER, num_jogadores INTEGER, tipo TEXT, status TEXT, valor_compra REAL)''')
       
       cursor.close()
       conexao.close()
@@ -45,47 +45,47 @@ class Jogo_venda(Jogo):
          conexao = sqlite3.connect(caminho_data)
          cursor = conexao.cursor()
          cursor.execute('''INSERT OR IGNORE INTO jogo_compra
-                       (id, nome, custo_aquisicao, data_aquisicao, descricao, idade_min, num_jogadores, tipo, status, valor_compra)
-                       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', (novo_jogo._id, novo_jogo._nome, novo_jogo._custo_aquisicao, novo_jogo._data_aquisicao, novo_jogo._descricao, novo_jogo._idade_min, novo_jogo._num_jogadores, novo_jogo._tipo, novo_jogo._status, novo_jogo._valor_compra))
+                       (id_produto, nome, custo_aquisicao, data_aquisicao, descricao, idade_min, num_jogadores, tipo, status, valor_compra)
+                       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', (novo_jogo._id_produto, novo_jogo._nome, novo_jogo._custo_aquisicao, novo_jogo._data_aquisicao, novo_jogo._descricao, novo_jogo._idade_min, novo_jogo._num_jogadores, novo_jogo._tipo, novo_jogo._status, novo_jogo._valor_compra))
          conexao.commit()
          cursor.close()
          conexao.close()
 
          print("\n Jogo criado\n")
 
-    def read(id):
+    def read(id_produto):
           '''Recebe o id do jogo alugável e retorna uma tupla com os dados dele.'''
           Jogo_venda.tabela_jogo_venda()
           conexao = sqlite3.connect(caminho_data)
           cursor = conexao.cursor()
-          cursor.execute('''SELECT * FROM jogo_compra WHERE id = ?''', (id, ))
+          cursor.execute('''SELECT * FROM jogo_compra WHERE id_produto = ?''', (id_produto, ))
           jogo_compravel = cursor.fetchone()
 
           cursor.close()
           conexao.close()
           return jogo_compravel
     
-    def update(id, nome_atributo, atributo_update):
+    def update(id_produto, nome_atributo, atributo_update):
           '''Recebe o id, o nome do atributo e o atributo update e atualiza o atributo com o  valor do update.'''
           Jogo_venda.tabela_jogo_venda()
           conexao = sqlite3.connect(caminho_data)
           cursor = conexao.cursor()
           cursor.execute(f'''UPDATE jogo_compra
                         SET {nome_atributo} = ?
-                        WHERE id = ?''', (atributo_update, id))
+                        WHERE id_produto = ?''', (atributo_update, id_produto))
           
           conexao.commit()
           cursor.close()
           conexao.close() 
     
-    def delete(id):
+    def delete(id_produto):
           '''Recebe o id do jogo alugável e atualiza seu status para inativo.'''
           Jogo_venda.tabela_jogo_venda()
           conexao = sqlite3.connect(caminho_data)
           cursor = conexao.cursor()
           cursor.execute('''UPDATE jogo_compra
                         SET status = ?
-                        WHERE id = ?''', ("inativo", id))
+                        WHERE id_produto = ?''', ("inativo", id_produto))
           
           conexao.commit()
           cursor.close()
