a43322a56844ff9ef3e228cb8e05ab2755489319
Jackson <samuel.jackson@aluno.ufca.edu.br>
2026-02-22 21:56:35 -0300

    FIx tupla-objeto Jogo Venda
    Fazendo a implementação do método tupla-objeto.

--- src/board_and_play_poo/modules/domain/compraveis.py
+++ src/board_and_play_poo/modules/domain/compraveis.py
@@ -11,7 +11,7 @@ class Jogo_venda(Jogo):
           self._valor_compra = valor_compra
 
     def __str__(self):
-         return f"ID: {self.id_produto}\nNome: {self.nome}\nCusto de aquisição: {self._custo_aquisicao}\nDescrição: {self._descricao}\nIdade mínima: {self._idade_min}\nNúmero de jogadores: {self._num_jogadores}\nTipo: {self.tipo}\nStatus: {self._status}"
+         return f"ID: {self._id_produto}\nNome: {self._nome}\nCusto de aquisição: {self._custo_aquisicao}\nDescrição: {self._descricao}\nIdade mínima: {self._idade_min}\nNúmero de jogadores: {self._num_jogadores}\nTipo: {self._tipo}\nStatus: {self._status}"
 
     def __eq__(self, outro):
          return self.id_produto == outro.id_produto
@@ -95,3 +95,8 @@ class Jogo_venda(Jogo):
           conexao.close()
 
           return "Sucesso! Jogo inativado."
+    
+    def tupla_objeto(tupla):
+         '''Transforma a tupla retornada no método read em um objeto'''
+         jogo_objeto = Jogo_venda(tupla[0], tupla[1], tupla[2], tupla[3], tupla[4], tupla[5], tupla[6], tupla[7], tupla[8], tupla[9])
+         return jogo_objeto
