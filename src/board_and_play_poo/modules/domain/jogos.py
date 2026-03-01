c0f9c85e7e4c5dac14d50d14d6f1946bfd1cb58c
Elilucio7 <eliluciofilho@gmail.com>
2026-02-28 23:52:42 -0300

    Refatorar jogo
    - Ajuste de métodos de repository_jogo para refletir repository_produto
    - Ajustes na classe jogo para melhor alocar atributos corretamente
    
    Co-Authored-By: Icaro Cavalcante <203451841+Icaro-Cavalcante@users.noreply.github.com>
    Co-Authored-By: Jackson <230462551+SJacksonML@users.noreply.github.com>

--- src/board_and_play_poo/modules/domain/jogos.py
+++ src/board_and_play_poo/modules/domain/jogos.py
@@ -1,7 +1,9 @@
-class Jogo():
+from .produtos import Produto
+
+class Jogo(Produto):
     '''Descreve os atributos que existem em todas as especificações de jogos.'''
-    def __init__(self, produto_id, etiqueta, genero, descricao, idade_min, num_jogadores, tipo_jogo, status, id=None):
-        self.produto_id = produto_id
+    def __init__(self, nome, codigo_barras, categoria, quantidade, etiqueta, genero, descricao, idade_min, num_jogadores, tipo_jogo, status, produto_id = None, jogo_id=None):
+        super().__init__(nome, codigo_barras, categoria, quantidade, produto_id)
         self.etiqueta = etiqueta
         self.genero = genero
         self.descricao = descricao
@@ -9,7 +11,7 @@ class Jogo():
         self.num_jogadores = num_jogadores
         self.tipo_jogo = tipo_jogo
         self.status = status
-        self.id = id
+        self.jogo_id = jogo_id
 
     def __str__(self):
-        return f"ID do jogo: {self.id}\nID de produto desse jogo: {self.produto_id}\nEtiqueta: {self.etiqueta}\nGênero do jogo: {self.genero}\nDescrição: {self.descricao}\nIdade mínima: {self.idade_min}\nNúmero mínimo de jogadores: {self.num_jogadores}\nTipo do jogo: {self.tipo_jogo}\nStatus: {self.status}"
+        return f"ID do jogo: {self.jogo_id}\nID de produto desse jogo: {self.produto_id}\nEtiqueta: {self.etiqueta}\nGênero do jogo: {self.genero}\nDescrição: {self.descricao}\nIdade mínima: {self.idade_min}\nNúmero de jogadores: {self.num_jogadores}\nTipo do jogo: {self.tipo_jogo}\nStatus: {self.status}"
