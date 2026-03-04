5caef393426985b7d3299bb6460d992eb48bee28
Jackson <samuel.jackson@aluno.ufca.edu.br>
2026-03-03 22:12:38 -0300

    Refatoração de Acessorio
    Implementação de acessorio de acordo com as mudanças em produto.

--- src/board_and_play_poo/modules/domain/acessorios.py
+++ src/board_and_play_poo/modules/domain/acessorios.py
@@ -2,8 +2,8 @@ from .produtos import Produto
 
 class Acessorio(Produto):
     '''Classe que cuida de todos os produtos do tipo acessorio.'''
-    def __init__(self, nome, codigo_barras, categoria, quantidade, tipo_acessorio, produto_id = None, acessorio_id = None):
-        super().__init__(nome, codigo_barras, categoria, quantidade, produto_id)
+    def __init__(self, nome, codigo_barras, categoria, tipo_acessorio, produto_id = None, acessorio_id = None):
+        super().__init__(nome, codigo_barras, categoria, produto_id)
         self.tipo_acessorio = tipo_acessorio
         self.acessorio_id = acessorio_id
 
