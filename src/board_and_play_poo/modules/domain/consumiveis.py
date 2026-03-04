f583c9d36586f878fe1ede31e9762ae93047e635
Jackson <samuel.jackson@aluno.ufca.edu.br>
2026-03-03 22:08:29 -0300

    Refatoração de Consumivel
    Implementadas as mudanças feitas em Produto.

--- src/board_and_play_poo/modules/domain/consumiveis.py
+++ src/board_and_play_poo/modules/domain/consumiveis.py
@@ -2,8 +2,8 @@ from .produtos import Produto
 
 class Consumivel(Produto):
     '''Classe que cuida dos consumíveis (alimentos prontos, bebidas)'''
-    def __init__(self, nome, codigo_barras, categoria, quantidade, data_validade, lote, restricoes, produto_id = None, consumivel_id = None):
-        super().__init__(nome, codigo_barras, categoria, quantidade, produto_id)
+    def __init__(self, nome, codigo_barras, categoria, data_validade, lote, restricoes, produto_id = None, consumivel_id = None):
+        super().__init__(nome, codigo_barras, categoria, produto_id)
         self.data_validade = data_validade
         self.lote = lote
         self.restricoes = restricoes
