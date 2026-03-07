3213464d130b1e15811f6a594a318f8e28180cdc
Elilucio7 <eliluciofilho@gmail.com>
2026-03-07 17:48:01 -0300

    refatorar transacao (1/?)
    - adição de algumas abstracts
    - correção de tabelas (remover status de transacao)
    - correção de repositório pra mudança de tabela

--- src/board_and_play_poo/modules/domain/transacoes.py
+++ src/board_and_play_poo/modules/domain/transacoes.py
@@ -7,35 +7,29 @@ from pathlib import Path
 caminho_data = "src/board_and_play_poo/data/dados.db"
 
 class Transacao(ABC):
-    def __init__(self, data_hora, valor_total, forma_pagamento, status_pagamento, tipo_transacao , id = None):
-        self.id = id
+    def __init__(self, comprovante, data_hora, valor_total, forma_pagamento, tipo_transacao , transacao_id = None):
+        self.transacao_id = transacao_id
+        self.comprovante = comprovante
         self.data_hora = data_hora
         self.valor_total = valor_total
         self.forma_pagamento = forma_pagamento
-        self.status_pagamento = status_pagamento
         self.tipo_transacao = tipo_transacao
 
     def __str__(self):
         return f"ID da transação: {self.id} | ID do cliente: {self.id_cliente} | ID do colaborador: {self.id_colaborador}\n"
-'''
+
     @abstractmethod
     def calcular_valor(self, obj_negocio):
         pass
-        try:
-            if obj_negocio.id_aluguel:
-                escolha = int(input("\n1 - Aluguel interno\n2 - Aluguel externo"))
-                match escolha:
-                    case 1:
-                        valor = Aluguel.calculo_aluguel_interno(int(input("\nDigite quantas sessões serão ofertadas: ")))
-                    case 2:
-                        valor = Aluguel.calculo_aluguel_externo(int(input("\nDigite a quantia de dias para locação: ")))
-                    case _:
-                        print("Escolha inválida, saindo de menu de cálculo.\n")
-            elif obj_negocio.id_venda:
-                valor = Venda.calcular_venda(int(input("Digite a quantia deste produto a ser comprado: ")))
-        except AttributeError:
-            return print("Tentativa de cálculo de valor falhou, tente repassar um aluguel/venda existente e ativo.")
-        return valor'''
+
+    @abstractmethod
+    def gerar_comprovante(self, obj_negocio): #????????????
+        pass
+
+    @abstractmethod
+    def tempo_atual(self, obj_negocio):
+        pass
+
 '''
     @abstractmethod
     def calcular_multa(self, obj_negocio):
