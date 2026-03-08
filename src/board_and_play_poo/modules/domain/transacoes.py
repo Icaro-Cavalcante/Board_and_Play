f96a90bbd040b9b205e4c847356d07b4ae3446ee
Elilucio7 <eliluciofilho@gmail.com>
2026-03-08 10:43:01 -0300

    docstrings
    docstrings em venda e transacao

--- src/board_and_play_poo/modules/domain/transacoes.py
+++ src/board_and_play_poo/modules/domain/transacoes.py
@@ -2,6 +2,7 @@ import sqlite3
 from abc import ABC, abstractmethod
 
 class Transacao(ABC):
+    """Classe abstrata que entrega atributos e métodos para Venda e Aluguel"""
     def __init__(self, comprovante, data_hora, valor_total, forma_pagamento, tipo_transacao , transacao_id = None):
         self._transacao_id = transacao_id
         self._comprovante = comprovante
