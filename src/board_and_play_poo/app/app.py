d88f72105e43947898f5630437d3a803b33a3907
Elilucio7 <eliluciofilho@gmail.com>
2026-03-08 11:01:25 -0300

    inicio

--- src/board_and_play_poo/app/app.py
+++ src/board_and_play_poo/app/app.py
@@ -59,58 +59,6 @@ class App():
                 case _:
                     print("Escolha inválida.\n")
 
-    def menu_jogo_aluguel():
-        '''Menu da classe jogo aluguel'''
-        while True:
-            while True:
-                try:
-                    print("\nJogos alugáveis")
-                    print("-" * 20)
-                    print("1 - Cadastrar\n2 - Consultar\n3 - Atualizar dados\n4 - Inativar\n5 - Voltar")
-                    escolha = int(input("Selecione uma opção: "))
-                    break
-                except ValueError:
-                    print("\nA opção deve ser um número inteiro.\n")
-            match escolha:
-                case 1:
-                    print("\n===Função em desenvolvimento===\n")
-                case 2:
-                    print("\n===Função em desenvolvimento===\n")
-                case 3:
-                    print("\n===Função em desenvolvimento===\n")
-                case 4:
-                    print("\n===Função em desenvolvimento===\n")
-                case 5:
-                    break
-                case _:
-                    print("Escolha inválida.\n")
-
-    def menu_jogo_venda():
-        '''Menu da classe jogo venda'''
-        while True:
-            while True:
-                try:
-                    print("\nJogos vendíveis")
-                    print("-" * 20)
-                    print("1 - Cadastrar\n2 - Consultar\n3 - Atualizar dados\n4 - Inativar\n5 - Voltar")
-                    escolha = int(input("Selecione uma opção: "))
-                    break
-                except ValueError:
-                    print("\nA opção deve ser um número inteiro.\n")
-            match escolha:
-                case 1:
-                    print("\n===Função em desenvolvimento===\n")
-                case 2:
-                    print("\n===Função em desenvolvimento===\n")
-                case 3:
-                    print("\n===Função em desenvolvimento===\n")
-                case 4:
-                    print("\n===Função em desenvolvimento===\n")
-                case 5:
-                    break
-                case _:
-                    print("Escolha inválida.\n")
-
     def menu_acessorio():
         '''Menu da classe acessorio'''
         while True:
---
+++ src/board_and_play_poo/app/menus/menu_jogo_aluguel.py
@@ -0,0 +1,29 @@
+
+class Menu_Jogo_Aluguel():
+    def menu_jogo_aluguel(self):
+            '''Menu da classe jogo aluguel'''
+            while True:
+                while True:
+                    try:
+                        print("\nJogos alugáveis")
+                        print("-" * 20)
+                        print("1 - Cadastrar\n2 - Consultar\n3 - Atualizar dados\n4 - Inativar\n5 - Voltar")
+                        escolha = int(input("Selecione uma opção: "))
+                        break
+                    except ValueError:
+                        print("\nA opção deve ser um número inteiro.\n")
+                match escolha:
+                    case 1:
+                        print("\n===Função em desenvolvimento===\n")
+                    case 2:
+                        print("\n===Função em desenvolvimento===\n")
+                    case 3:
+                        print("\n===Função em desenvolvimento===\n")
+                    case 4:
+                        print("\n===Função em desenvolvimento===\n")
+                    case 5:
+                        break
+                    case _:
+                        print("Escolha inválida.\n")
+
+    def cadastro_jogo_aluguel():
