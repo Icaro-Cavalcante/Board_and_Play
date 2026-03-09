af9b53b9f16b854028bab7149680276c21ab2c97
Elilucio7 <eliluciofilho@gmail.com>
2026-03-08 23:40:05 -0300

    Implementação da classe menu contrato
    - `Implementação da classe menu_contrato com seus principais métodos`
    
    Co-Authored-By: Icaro Cavalcante <203451841+Icaro-Cavalcante@users.noreply.github.com>
    Co-Authored-By: Jackson <samuel.jacksonjml@gmail.com>

--- src/board_and_play_poo/app/menus/menu_contrato.py
+++ src/board_and_play_poo/app/menus/menu_contrato.py
@@ -1,5 +1,18 @@
+from src.board_and_play_poo.repositories.repository_aluguel import RepositoryAluguel
+from src.board_and_play_poo.repositories.repository_item_aluguel import RepositoryItemAluguel
+from src.board_and_play_poo.modules.domain.alugueis import Aluguel 
+from src.board_and_play_poo.database.database import Database
+from src.board_and_play_poo.database.tabelas import Tabela
+
+tb = Tabela()
+db = Database("real")
+tb.create_table(db) # Criando tabelas do db de test
+
+aluguel_repo = RepositoryAluguel(db, tb)
+item_repo = RepositoryItemAluguel(db, tb)
+
 class MenuContrato:    
-    def menu_contrato():
+    def menu_contrato(self):
         '''Menu das consultas dos contratos de aluguel do sistema'''
         while True:
             while True:
@@ -13,14 +26,61 @@ class MenuContrato:
                     print("\nA opção deve ser um número inteiro.\n")
             match escolha:
                 case 1:
-                    print("\n===Função em desenvolvimento===\n")
+                    MenuContrato.menu_consultar_id()
                 case 2:
-                    print("\n===Função em desenvolvimento===\n")
+                    MenuContrato.menu_consultar_status("ATIVO")
                 case 3:
-                   print("\n===Função em desenvolvimento===\n")
+                    MenuContrato.menu_consultar_status("ALTERADO")
                 case 4:
-                    print("\n===Função em desenvolvimento===\n")
+                    MenuContrato.menu_consultar_status("FECHADO")
                 case 5:
                     break
                 case _:
                     print("Escolha inválida.\n")
+
+    def menu_consultar_id():
+        '''Menu para interagir com a função de consultar por id'''
+        while True:
+            try:
+                id = int(input("Insira o id que deseja buscar: "))
+                break
+            except ValueError:
+                print("\nId precisa ser um número inteiro!\n")
+        contrato_consulta = aluguel_repo.read(id)
+        if contrato_consulta:
+            while True:
+                while True:
+                    try:
+                        print(f"\nContrato {contrato_consulta.id}")
+                        print("-" * 20)
+                        print("1 - Consultar atributos\n2 - Consultar itens\n3 - Sair")
+                        escolha = int(input("Selecione uma opção: "))
+                        break
+                    except ValueError:
+                        print("\nA opção deve ser um número inteiro.\n")
+                match escolha:
+                    case 1:
+                        print(contrato_consulta)
+                    case 2:
+                        tuplas = item_repo.buscar_por_aluguel(contrato_consulta.id)
+                        for tupla in tuplas:
+                            print("")
+                            item_repo.imprimir_dados(tupla)
+                            print("")
+                    case 3:
+                        break
+                    case _:
+                        print("Escolha inválida.\n")
+        else:
+            print("Não foi possível conectar")
+            
+    def menu_consultar_status(status):
+        '''Menu para interagir com os contratos do status recebido'''
+        contrato_consulta = (aluguel_repo.read_especifico(status))
+        if contrato_consulta:
+            for contrato in contrato_consulta:
+                print("")
+                print(contrato)
+                print("")
+        else:
+            print("Não foi possível achar contratos.")
