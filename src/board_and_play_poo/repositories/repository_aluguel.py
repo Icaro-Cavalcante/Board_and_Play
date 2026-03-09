01bfa0717878a73653a96623fc98c04e6d50563f
Elilucio7 <eliluciofilho@gmail.com>
2026-03-08 23:37:27 -0300

    fix alugueis
    - `Retirada herança de atributos de Transacao por Aluguel`
    - `Implementação do método read_especifico`
    - `Implementação do método tupla_objeto`
    - `Aprimoramento do read`
    
    Co-Authored-By: Icaro Cavalcante <203451841+Icaro-Cavalcante@users.noreply.github.com>
    Co-Authored-By: Jackson <samuel.jacksonjml@gmail.com>

--- src/board_and_play_poo/repositories/repository_aluguel.py
+++ src/board_and_play_poo/repositories/repository_aluguel.py
@@ -1,4 +1,5 @@
 from sqlalchemy import text
+from src.board_and_play_poo.modules.domain.alugueis import Aluguel
 
 class RepositoryAluguel():
     '''Classe que realiza as operações do banco de dados relacionadas a alugueis'''
@@ -28,11 +29,34 @@ class RepositoryAluguel():
         conexao = self.database.conectar() # Estabelecendo a conexão
         if conexao: 
             query = text (f"""SELECT * FROM alugueis WHERE id = :id""")
-            aluguel = conexao.execute (query, {"id": id, } # query
+            tupla = conexao.execute (query, {"id": id, } # query
             ).first()
-            return aluguel # Aluguel é retornado
-        return None # Caso não, None é retornado
+            if tupla: # Se a tupla existir
+                # Criando um objeto
+                aluguel = self.tupla_objeto(tupla)
+                return aluguel # Aluguel é retornado
+            else:
+                return None
+        else:
+            return None # Caso não, None é retornado
+        
+    def read_especifico(self, status):
+        conexao = self.database.conectar()
+        if conexao:
+            query = text(f"""SELECT * FROM alugueis WHERE status = :status""")
+            tupla = conexao.execute (query, {"status": status,}).all()
+            if tupla:
+                for tuple in tupla:
+                    lista = []
+                    aux = self.tupla_objeto(tuple)
+                    lista.append(aux)
+                return lista
+            else:
+                return None
     
+    def tupla_objeto(self, tupla):
+        return Aluguel(tupla[4], tupla[5], tupla[6], tupla[7], tupla[8], tupla[3], tupla[2], tupla[1], tupla[0])
+
     def update(self, id, nome_atributo, atributo_update):
         '''Recebe o ID de um aluguel, o nome do atributo e o atributo atualizado e atualiza o atributo no banco de dados do ambiente selecionado'''
         # Nota 1: nome_atributo deve ser passado a partir de um dicionario. Exemplo dic = {1: "nome"}... nome deve ser passado como parâmetro e o usuário não pode passar nada que esteja fora dos atributos do dicionário.
